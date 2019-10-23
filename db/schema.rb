dc3750ddfe659e526f40e9b07a6837bd8a5956b6
Philip Cox <philcogm@gmail.com>
2019-10-23 00:18:25 +0000

    fixed lat and lng column display in console

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_10_22_222109) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
